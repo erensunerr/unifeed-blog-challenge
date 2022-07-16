d6fcdd4a7bad454d8b028e68f8fb4a5a1d047f45
Eren <gerensuner@gmail.com>
2022-07-16 12:27:23 +0300

    new article variant added now adapts better to desktop

--- src/App.js
+++ src/App.js
@@ -9,17 +9,21 @@ import {articles} from './data'
 
 function App() {
   console.log(articles)
+  const sw = window.screen.width;
+  console.log(sw);
   return (
     <section className="container mx-auto px-6 py-4 block">
       <Header />
 
       <section className="py-16 sm:py-28 grid grid-cols-1 sm:grid-cols-3 gap-6">
         {
-          articles.map((article, i) => <Article {...article} key={i}/>)
+          articles.map((article, i) =>
+            <Article {...article} key={i} variant={i === 0 && sw > 640 && "horizontal"}/>
+          )
         }
       </section>
 
-      <div className="flex flex-wrap justify-between items-center">
+      <div className="flex flex-wrap justify-between items-center mb-8">
         <img src={logo} alt=""/>
         <p className="text-neutral-600">
           Unifeed© All Copyrights Not Reserved
--- src/Article.js
+++ src/Article.js
@@ -1,30 +1,66 @@
 import React from 'react';
 
 
-function Article({imgSrc, title, description, author}) {
-  return (
-    <div className="mt-6 text-neutral-800 sm:first:col-span-full ">
-      <div className="hover:underline cursor-pointer">
-        <img src={imgSrc} alt="" className="min-w-full bg-contain aspect-video"/>
-        <h2 className="text-2xl mt-6 font-semibold">
-          {title}
-        </h2>
+function Article({imgSrc, title, description, author, variant}) {
+  // variant: "horizontal" | "vertical"
 
-        <p className="mt-3 text-neutral-600">
-          {description}
-        </p>
-      </div>
-
-
-      <div className="mt-3 flex items-center hover:underline cursor-pointer">
-        <img src={author.imgSrc} alt="" className="rounded-full w-12"/>
-        <div className="ml-4">
-          <h4 className="text-sm font-semibold">{author.name}</h4>
-          <p className="text-xs text-neutral-600">{author.title}</p>
+  switch (variant) {
+    case "horizontal":
+      return (
+      <>
+        <div className="col-span-1 cursor-pointer">
+          <img src={imgSrc} alt="" className="h-full object-cover rounded aspect-video"/>
         </div>
-      </div>
-    </div>
-  );
+
+        <div className="text-neutral-800 col-span-2 flex cursor-pointer ml-2">
+          <div className="">
+            <div className="hover:underline">
+              <h2 className="text-2xl mt-6 font-semibold">
+                {title}
+              </h2>
+
+              <p className="mt-3 text-neutral-600">
+                {description}
+              </p>
+            </div>
+
+            <div className="mt-3 flex items-center hover:underline hover:hue-rotate-[360deg] transition-all duration-1000 cursor-pointer">
+              <img src={author.imgSrc} alt="" className="rounded-full w-12"/>
+              <div className="ml-4">
+                <h4 className="text-sm font-semibold">{author.name}</h4>
+                <p className="text-xs text-neutral-600">{author.title}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+      )
+    default:// vertical variant is the default
+      return (
+        <div className="mt-6 text-neutral-800">
+          <div className="hover:underline cursor-pointer">
+            <img src={imgSrc} alt="" className="min-w-full bg-contain aspect-video"/>
+            <h2 className="text-2xl mt-6 font-semibold">
+              {title}
+            </h2>
+
+            <p className="mt-3 text-neutral-600">
+              {description}
+            </p>
+          </div>
+
+
+          <div className="mt-3 flex items-center hover:underline hover:hue-rotate-[360deg] transition-all duration-1000 cursor-pointer">
+            <img src={author.imgSrc} alt="" className="rounded-full w-12"/>
+            <div className="ml-4">
+              <h4 className="text-sm font-semibold">{author.name}</h4>
+              <p className="text-xs text-neutral-600">{author.title}</p>
+            </div>
+          </div>
+        </div>
+      )
+
+  }
 }
 
 export default Article;
